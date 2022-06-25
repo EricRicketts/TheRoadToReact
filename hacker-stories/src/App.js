927c6fa607411696b0437869c07a1ddc4fd8cf1a
Eric Ricketts <eric_ricketts@me.com>
2022-06-25 11:09:18 -0400

    add simple title text using JSX

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,9 +1,12 @@
 import * as React from 'react';
 
 function App() {
+    const title = "React";
   return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Hello {title}</h1>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text"/>
       </div>
   );
 }
