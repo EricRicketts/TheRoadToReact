1135124ae9d09d065f288a0bb6820e3d36e36239
Eric Ricketts <eric_ricketts@me.com>
2022-06-25 14:40:52 -0400

    iterate through an array of objects to create unordered lists in React

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useLayoutEffect} from "react";
 
 function App() {
     function getWelcome() {
@@ -8,13 +9,42 @@ function App() {
         }
     }
 
+    const list = [
+        {
+            title: 'React',
+            url: 'https://reactjs.org',
+            author: 'Jordan Walke',
+            num_comments: 3,
+            points: 4,
+            objectID: 0
+        },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 1
+        },
+    ];
+
   return (
       <div>
         <h1>
             {getWelcome().greeting} {getWelcome().title}
         </h1>
-
-        <label htmlFor="search">Search: </label>
+          {
+            list.map(listItem => {
+                return <ul key={listItem.objectID}>
+                    <li>{listItem.title}</li>
+                    <li>{listItem.url}</li>
+                    <li>{listItem.author}</li>
+                    <li>{listItem.num_comments}</li>
+                    <li>{listItem.points}</li>
+                </ul>
+            })
+          }
+          <label htmlFor="search">Search: </label>
         <input id="search" type="text"/>
       </div>
   );
