3ca79f28770b82edfd16e49a7b147f9e5902441c
Eric Ricketts <eric_ricketts@me.com>
2022-07-01 11:07:49 -0400

    modify the InputWithLabel Component to include imperative code on setting the autoFocus

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -21,17 +21,34 @@ const stories = [
     },
 ];
 // below <>, </> pair are react fragments, we could have used <React.Fragment> & </React.Fragment>
-const InputWithLabel = ({id, value, type = 'text', onInputChange, children}) => (
-   <>
-       <label htmlFor={id}>{children}</label>
-       <input
-           id={id}
-           type={type}
-           value={value}
-           onChange={onInputChange}
-       />
-   </>
-);
+const InputWithLabel = ({
+    id,
+    value,
+    type = 'text',
+    onInputChange,
+    isFocused,
+    children
+}) => {
+    const inputRef = React.useRef();
+    React.useEffect(() => {
+        if (isFocused && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [isFocused]);
+    return (
+        <>
+            <label htmlFor={id}>{children}</label>
+            &nbsp;
+            <input
+                ref={inputRef}
+                id={id}
+                type={type}
+                value={value}
+                onChange={onInputChange}
+            />
+        </>
+    );
+}
 
 const List = ({list}) => (
     <ul>
