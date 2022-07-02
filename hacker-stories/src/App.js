b87cb53303e70468fe28961f74911ddd372ff853
Eric Ricketts <eric_ricketts@me.com>
2022-07-02 19:12:45 -0400

    add asynchronous fetching of mock data

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,91 +1,69 @@
 import * as React from 'react';
 
-const getWelcome = () => ({ greeting: "Hey Ho", title: "React" });
-
-const stories = [
+const initialStories = [
     {
         title: 'React',
-        url: 'https://reactjs.org',
+        url: 'https://reactjs.org/',
         author: 'Jordan Walke',
         num_comments: 3,
         points: 4,
-        objectID: 0
+        objectID: 0,
     },
     {
         title: 'Redux',
-        url: 'https://redux.js.org',
+        url: 'https://redux.js.org/',
         author: 'Dan Abramov, Andrew Clark',
         num_comments: 2,
         points: 5,
-        objectID: 1
+        objectID: 1,
     },
 ];
-// below <>, </> pair are react fragments, we could have used <React.Fragment> & </React.Fragment>
-const InputWithLabel = ({
-    id,
-    value,
-    type = 'text',
-    onInputChange,
-    isFocused,
-    children
-}) => {
-    const inputRef = React.useRef();
-    React.useEffect(() => {
-        if (isFocused && inputRef.current) {
-            inputRef.current.focus();
-        }
-    }, [isFocused]);
-    return (
-        <>
-            <label htmlFor={id}>{children}</label>
-            &nbsp;
-            <input
-                ref={inputRef}
-                id={id}
-                type={type}
-                value={value}
-                onChange={onInputChange}
-            />
-        </>
+
+const getAsyncStories = () =>
+    new Promise((resolve) =>
+        setTimeout(
+            () => resolve({ data: { stories: initialStories } }),
+            2000
+        )
     );
-}
 
-const List = ({list}) => (
-    <ul>
-        {list.map((item) => (
-            <Item key={item.objectID} item={item} />
-        ))}
-    </ul>
-);
+const useSemiPersistentState = (key, initialState) => {
+    const [value, setValue] = React.useState(
+        localStorage.getItem(key) || initialState
+    );
 
-const Item = ({item}) => (
-    <li>
-        <span>
-            <a href={item.url}>{item.title}</a>
-        </span>
-        <span> {item.author}</span>
-        <span> {item.num_comments}</span>
-        <span> {item.points}</span>
-    </li>
-);
+    React.useEffect(() => {
+        localStorage.setItem(key, value);
+    }, [value, key]);
+
+    return [value, setValue];
+};
+
 const App = () => {
-    const useStorageState = (key, initialState) => {
-        const [value, setValue] = React.useState(
-            localStorage.getItem(key) || initialState
+    const [searchTerm, setSearchTerm] = useSemiPersistentState(
+        'search',
+        'React'
+    );
+
+    const [stories, setStories] = React.useState([]);
+
+    React.useEffect(() => {
+        getAsyncStories().then((result) => {
+            setStories(result.data.stories);
+        });
+    }, []);
+
+    const handleRemoveStory = (item) => {
+        const newStories = stories.filter(
+            (story) => item.objectID !== story.objectID
         );
 
-        React.useEffect(() => {
-            localStorage.setItem(key, value)
-        }, [key, value]);
-
-        return [value, setValue];
+        setStories(newStories);
     };
-
-    const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
     const handleSearch = (event) => {
         setSearchTerm(event.target.value);
-    }
+    };
 
     const searchedStories = stories.filter((story) =>
         story.title.toLowerCase().includes(searchTerm.toLowerCase())
@@ -93,17 +71,81 @@ const App = () => {
 
     return (
         <div>
-            <h1>{getWelcome().greeting} {getWelcome().title}</h1>
+            <h1>My Hacker Stories</h1>
+
             <InputWithLabel
                 id="search"
-                label="Search"
                 value={searchTerm}
+                isFocused
                 onInputChange={handleSearch}
-            ><strong>Search: </strong></InputWithLabel>
-            <hr/>
-            <List list={searchedStories} />
+            >
+                <strong>Search:</strong>
+            </InputWithLabel>
+
+            <hr />
+
+            <List list={searchedStories} onRemoveItem={handleRemoveStory} />
         </div>
     );
-}
+};
+
+const InputWithLabel = ({
+                            id,
+                            value,
+                            type = 'text',
+                            onInputChange,
+                            isFocused,
+                            children,
+                        }) => {
+    const inputRef = React.useRef();
+
+    React.useEffect(() => {
+        if (isFocused && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [isFocused]);
+
+    return (
+        <>
+            <label htmlFor={id}>{children}</label>
+            &nbsp;
+            <input
+                id={id}
+                ref={inputRef}
+                type={type}
+                value={value}
+                onChange={onInputChange}
+            />
+        </>
+    );
+};
+
+const List = ({ list, onRemoveItem }) => (
+    <ul>
+        {list.map((item) => (
+            <Item
+                key={item.objectID}
+                item={item}
+                onRemoveItem={onRemoveItem}
+            />
+        ))}
+    </ul>
+);
+
+const Item = ({ item, onRemoveItem }) => (
+    <li>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
+    </li>
+);
 
 export default App;
