10ff835ccc2e20219ce2bb5fc42b36ca64cad8ac
Eric Ricketts <eric_ricketts@me.com>
2022-07-03 05:32:08 -0400

    conditional rendering case if there is an error

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -47,13 +47,14 @@ const App = () => {
 
     const [stories, setStories] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(false);
+    const [isError, setIsError] = React.useState(false);
 
     React.useEffect(() => {
         setIsLoading(true);
         getAsyncStories().then((result) => {
             setStories(result.data.stories);
             setIsLoading(false);
-        });
+        }).catch(() => setIsError(true));
     }, []);
 
     const handleRemoveStory = (item) => {
@@ -86,6 +87,9 @@ const App = () => {
             </InputWithLabel>
 
             <hr />
+
+            {isError && <p>Something went wrong ...</p>}
+
             {isLoading ? (
                 <p>Loading ...</p>
             ) : (
